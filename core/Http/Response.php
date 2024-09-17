520d8fc75493c2cbfed71e7b54fdcf9c493e6933
Mustakim Farooqui <fmustakim7@gmail.com>
2024-09-17 17:43:08 -0400

    added necessary comments and css build

--- core/Http/Response.php
+++ core/Http/Response.php
@@ -5,6 +5,14 @@ namespace Core\Http;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class Response extends SymfonyResponse {
+
+    /**
+     * Create a new response instance.
+     *
+     * @param string $content
+     * @param int $status
+     * @param array $headers
+     */
     public function __construct(string $content = '', int $status = 200, array $headers = []) {
         parent::__construct($content, $status, $headers);
     }
--- core/Validation/Validator.php
+++ core/Validation/Validator.php
@@ -10,12 +10,26 @@ class Validator {
 
     private ValidatorFactory $validator;
 
+    /**
+     * Initializes the validator with a default translator.
+     *
+     * The translator is initialized with the English language and an empty
+     * array loader. You can replace the translator with your own implementation
+     * if needed.
+     */
     public function __construct() {
         $loader = new ArrayLoader();
         $translator = new Translator($loader, 'en');
         $this->validator = new ValidatorFactory($translator);
     }
 
+    /**
+     * Validates the given data against the given rules.
+     *
+     * @param array $data The data to validate.
+     * @param array $rules The validation rules.
+     * @return array An array of validation errors, or an empty array if the data is valid.
+     */
     public function validate(array $data, array $rules) {
         $validator = $this->validator->make($data, $rules);
         if ($validator->fails()) {
